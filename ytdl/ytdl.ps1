8321098e3f268cec8e3f9b147ee3eaeb7f260b39
Simo Saarinen <20212250+simbrr@users.noreply.github.com>
2022-01-16 20:10:51 +0200

    Added help texts to [?]

--- ytdl/ytdl.ps1
+++ ytdl/ytdl.ps1
@@ -3,13 +3,18 @@ $ytLink = Read-Host -Prompt 'Youtube url'
 $Title = "Youtube Downloader"
 $Info = "Do you want to download Audio or Video?"
 
-$options = [System.Management.Automation.Host.ChoiceDescription[]] @("&Audio", "&Video")
+$options = @(
+    [System.Management.Automation.Host.ChoiceDescription]::new("&Audio", "Downloads as MP3")
+    [System.Management.Automation.Host.ChoiceDescription]::new("&Video", "Downloads as MP4")
+)
 [int]$defaultchoice = 1
+
 $opt = $host.UI.PromptForChoice($Title , $Info , $Options, $defaultchoice)
+
 switch($opt)
 {    
-0 {C:\ProgramData\chocolatey\bin\yt-dlp.exe $ytLink -x --audio-format mp3}
-1 {C:\ProgramData\chocolatey\bin\yt-dlp.exe $ytLink --recode-video mp4}
+    0 {C:\ProgramData\chocolatey\bin\yt-dlp.exe $ytLink -x --audio-format mp3}
+    1 {C:\ProgramData\chocolatey\bin\yt-dlp.exe $ytLink --recode-video mp4}
 } 
 
 Read-Host -Prompt  "All done. Press enter to exit."
